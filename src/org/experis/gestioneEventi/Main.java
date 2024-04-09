ef5ebc3d4e6df3f233a6cdbe5eada29b63ae2d01
peppegerace <peppegerace47@gmail.com>
2024-04-09 12:31:03 +0200

    [add] while loop to receive data from user

--- src/org/experis/gestioneEventi/Main.java
+++ src/org/experis/gestioneEventi/Main.java
@@ -1,8 +1,8 @@
 package org.experis.gestioneEventi;
 
-import jdk.nashorn.internal.runtime.regexp.joni.ScanEnvironment;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -10,9 +10,57 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+//        System.out.print("Inserire titolo evento: ");
+//        String titolo = scan.nextLine();
+//
+//        System.out.print("Inserire data evento (formato yyyy-mm-dd): ");
+//        LocalDate data = LocalDate.parse(scan.nextLine());
+//
+//        System.out.print("Inserire la capienza: ");
+//        int capienza = Integer.parseInt(scan.nextLine());
+//
+//        Evento concerto = new Evento(titolo, data, capienza);
+//        System.out.println(concerto);
 
-        LocalDate dataEvento = LocalDate.of(2025,1,9);
-        Evento concerto = new Evento("Depeche Mode", dataEvento, 10000);
-        System.out.println(concerto);
+
+        String titolo = "";
+        LocalDate data = null;
+        int capienza = 0;
+
+        boolean datiValidi = false;
+
+        while (!datiValidi) {
+            try {
+                System.out.print("Inserire titolo evento: ");
+                titolo = scan.nextLine();
+                if (titolo.isEmpty()) {
+                    throw new IllegalArgumentException("Il titolo non può essere vuoto");
+                }
+
+                System.out.print("Inserire data evento (formato yyyy-mm-dd): ");
+                data = LocalDate.parse(scan.nextLine());
+                if (data.isBefore(LocalDate.now())) {
+                    throw new IllegalArgumentException("La data dell'evento è già passata");
+                }
+
+                System.out.print("Inserire la capienza: ");
+                capienza = Integer.parseInt(scan.nextLine());
+                if (capienza <= 0) {
+                    throw new IllegalArgumentException("La capienza deve essere superiore a 0");
+                }
+                datiValidi = true;
+            } catch (DateTimeParseException e) {
+                System.out.println("Errore: " + e.getMessage() + " Inserisci una data valida nel formato yyyy-mm-dd");
+            } catch (NumberFormatException e) {
+                System.out.println("Errore: Formato non valido. Inserire un numero intero superiore a 0");
+            } catch (IllegalArgumentException e) {
+                System.out.println("Errore: " + e.getMessage() + " Riprova.");
+            }
+        }
+
+        Evento event = new Evento(titolo, data, capienza);
+        System.out.println(event);
+
+        scan.close();
     }
 }
